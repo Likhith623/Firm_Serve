27a433c14c67c8c3ba179770c4fa1c0d1a357f04
LALITH CHOWDARY <154964275+LalithChowdary@users.noreply.github.com>
2025-03-31 12:22:51 +0530

    Lalith (#6)
    * added prisma
    
    * connection succesfull with db

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,37 @@
 export default function Home() {
   return <div>hi this is lalith</div>;
 }
+
+// // app/page.tsx
+// import { PrismaClient } from "@prisma/client";
+// import React from "react";
+
+// // It's recommended to instantiate PrismaClient outside of the component
+// // if you're using it in server components, to avoid creating too many instances.
+// const prisma = new PrismaClient();
+
+// export default async function Home() {
+//   // Fetch data directly in the server component
+//   const clients = await prisma.client.findMany();
+
+//   return (
+//     <div>
+//       <table>
+//         <thead>
+//           <tr>
+//             <th>Name</th>
+//             {/* Add other headers if needed */}
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {clients.map((cli) => (
+//             <tr key={cli.client_id}>
+//               <td>{cli.name}</td>
+//               {/* Render other client fields as needed */}
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// }
