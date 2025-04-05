924cf101d3cf3eec4b0a7d725e1b61bff104c004
LALITH CHOWDARY <154964275+LalithChowdary@users.noreply.github.com>
2025-04-05 15:48:20 +0530

    added email auth data (#20)

--- app/admin/clients/page.tsx
+++ app/admin/clients/page.tsx
@@ -1,3 +1,38 @@
+import { prisma } from "@/prisma/client";
+import React from "react";
+
 export default async function Home() {
-  return <div>Admin clients</div>;
+  const clients = await prisma.client.findMany({
+    include: {
+      client_auth: true, // Include related User data
+    },
+  });
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Phone No</th>
+            <th>Email</th>
+            {/* Add other headers if needed */}
+          </tr>
+        </thead>
+        <tbody>
+          {clients.map((client) => (
+            <tr key={client.client_id}>
+              <td>{client.name}</td>
+              <td>{client.address}</td>
+              <td>{client.phone_no}</td>
+              <td>{client.client_auth.email}</td>
+              <td></td>
+              {/* Render other client fields as needed */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
--- app/admin/staff/page.tsx
+++ app/admin/staff/page.tsx
@@ -1,3 +1,47 @@
+import { prisma } from "@/prisma/client";
+import React from "react";
+
 export default async function Home() {
-  return <div>Admin staff</div>;
+  const clients = await prisma.staff.findMany({
+    include: {
+      staff_auth: true, // Include related User data
+    },
+  });
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Phone No</th>
+            <th>Email</th>
+            <th>Bar Number</th>
+            <th>Designation</th>
+            <th>Specialisation</th>
+            <th>Experience</th>
+            <th>Role</th>
+            {/* Add other headers if needed */}
+          </tr>
+        </thead>
+        <tbody>
+          {clients.map((staff) => (
+            <tr key={staff.staff_id}>
+              <td>{staff.name}</td>
+              <td>{staff.address}</td>
+              <td>{staff.phone_no}</td>
+              <td>{staff.staff_auth.email}</td>
+              <td>{staff.bar_number}</td>
+              <td>{staff.designation}</td>
+              <td>{staff.specialisation}</td>
+              <td>{staff.experience}</td>
+              <td>{staff.s_role}</td>
+              {/* Render other client fields as needed */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
